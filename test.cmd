d7564911c4205283325a6766326afa4cb89c1d03
flame99999 <flame99999@aliyun.com>
2019-04-16 16:53:32 +0900

    Update test

--- test.cmd
+++ test.cmd
@@ -5,7 +5,7 @@ git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 set path=%~dp0webrtcBuild\depot_tools;%path%
 set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 set GYP_MSVS_VERSION = 2017
-set GYP_MSVS_OVERRIDE_PATH = C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise
+set GYP_MSVS_OVERRIDE_PATH = C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional
 set GYP_GENERATORS=msvs-ninja,ninja
 echo %path%
 call gclient
