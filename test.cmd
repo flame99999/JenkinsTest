97492af565ece487d7fd4ee9e5ac0a46ef34a52c
flame99999 <flame99999@aliyun.com>
2019-04-16 14:58:45 +0900

    Update test.cmd

--- test.cmd
+++ test.cmd
@@ -2,12 +2,12 @@ pushd %~dp0
 mkdir webrtcBuild
 cd webrtcBuild
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-set path=%~dp0webrtBuild\depot_tools;%path%
+set path=%~dp0webrtcBuild\depot_tools;%path%
 echo %path%
 gclient
-cd ../
 fetch --nohooks webrtc
 gclient sync
 git checkout branch-heads/72
 gn gen out/TestBuild
 ninja -C out/TestBuild
+pause
